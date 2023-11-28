de7923b2a31815f55bc63afb745de591ae07862e
micalux <micalux@gmail.com>
2023-11-28 17:12:48 +0100

    final edits

---
+++ data.js
@@ -0,0 +1,26 @@
+export const bergenHikes = [
+        {
+            name: "Fløyen",
+            image: "images/home-01.jpg",
+            lenght: "10.2 km - Est. 5 h",
+            description: `Fløibanen is one of Norway's most visited attractions. 
+                        At the top of Fløyen you will find a large and varied hiking area, 
+                        spectacular views of the city.`
+        },
+        {
+            name: "Ulriken",
+            image: "images/home-02.jpg",
+            lenght: "Length: 3.5 km - Est. 2 h 30 m",
+            description: `Ulriken is the highest of the Seven Mountains that surround 
+                        the city of Bergen, Norway. It has a height of 643 meters (2,110 ft) 
+                        above sea level.`
+        },
+        {
+            name: "Landåsfjellet",
+            image: "images/home-03.jpg",
+            lenght: "Length: 4.3 km - Est. 1 h 25 m",
+            description: `Landåsfjellet is a lower plateau south of Ulriken/Haugavarden 
+                        which offers numerous trails, and is almost a separate hiking 
+                        region by its own.`
+        }
+    ]
